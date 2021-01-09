11850a1a2e6755424cef6c037a2efe772fefe5a5
Maksym Bilashenko <mbilashenko@conceptsoft.io>
2021-01-09 14:36:30 +0200

    Bootcamp basic CRUD

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// const logger = require('./middleware/logger');
 const colors = require('colors');
 const connectDB = require('./config/db');
 
@@ -13,11 +12,16 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-const { yellow } = require('colors');
 
 const app = express();
 
-app.use(morgan);
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -26,12 +30,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server & exit process
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
